83e21d858198e96e341933c32b463f204ec0a07d
Benjamin Tang <benjamin.tang.hz@gmail.com>
2020-02-21 22:51:06 +0800

    profile page mockup

--- src/pages/ProfileTab.tsx
+++ src/pages/ProfileTab.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   IonContent,
   IonHeader,
@@ -10,10 +10,24 @@ import {
   IonItemGroup,
   IonItem,
   IonAvatar,
-  IonText
-} from "@ionic/react";
-import { RefresherEventDetail } from "@ionic/core";
-import "./ProfileTab.css";
+  IonText,
+  IonIcon,
+  IonList,
+  IonListHeader,
+  IonItemDivider,
+  IonSegment,
+  IonSegmentButton,
+  IonRange,
+  IonInput,
+} from '@ionic/react';
+
+import { chevronForwardOutline } from 'ionicons/icons';
+// @ts-ignore
+import NumericInput from 'react-numeric-input';
+import { RefresherEventDetail } from '@ionic/core';
+// @ts-ignore
+import InputNumber from 'rc-input-number';
+import './ProfileTab.css';
 
 const ProfileTab: React.FC = () => {
   return (
@@ -24,32 +38,83 @@ const ProfileTab: React.FC = () => {
             <IonTitle>
               <span
                 className="font-title"
-                style={{ fontWeight: 900, fontSize: "1.2rem" }}
+                style={{ fontWeight: 900, fontSize: '1.2rem' }}
               >
                 squad
               </span>
             </IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonItem lines="none" className="ion-padding">
-          <IonItem lines="none" slot="end">
-            <IonAvatar style={{ width: "64px", height: "64px" }}>
-              <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
-            </IonAvatar>
-          </IonItem>
-          <IonItem lines="none" className="leading-snug">
-            <IonText
-              style={{
-                textAlign: "center",
-                width: "100%",
-                lineHeight: "1"
-              }}
-            >
-              <h2>Benjamin Tang</h2>
-              <span className="text-gray-600">Edit Profile</span>
-            </IonText>
-          </IonItem>
-        </IonItem>
+        <div className="shadow-md rounded-b-lg py-8 px-8 w-full flex">
+          <IonText className="text-center w-full flex-1 mr-4">
+            <h2 className="font-bold">Benjamin Tang</h2>
+            <span className="text-gray-500">Yoga Beginner</span>
+          </IonText>
+          <IonAvatar className="h-20 w-20">
+            <img
+              alt="profile"
+              src="https://scontent.fsin3-1.fna.fbcdn.net/v/t1.0-9/67214406_2642116952486182_4117217862546882560_o.jpg?_nc_cat=109&_nc_ohc=pOKfPjd5_s8AX9oK6S0&_nc_ht=scontent.fsin3-1.fna&oh=afc45f992a09dc7f1524b37f8ee78e67&oe=5F020112"
+            />
+          </IonAvatar>
+        </div>
+
+        <div className="h-8" />
+        <div className="flex flex-col px-10">
+          <div className="text-2xl font-bold">
+            Basic Info{' '}
+            <span className="text-base text-gray-500 font-normal">
+              &nbsp;edit
+            </span>
+          </div>
+          <div className="mt-4">
+            <div className="text-gray-700 mb-1">Gender</div>
+            <IonSegment>
+              <IonSegmentButton value="male">he/him</IonSegmentButton>
+              <IonSegmentButton value="female">she/her</IonSegmentButton>
+              <IonSegmentButton value="other">they/them</IonSegmentButton>
+            </IonSegment>
+          </div>
+          <div className="mt-4">
+            <div className="text-gray-700 mb-1">Overall Fitness Level</div>
+            <IonSegment>
+              <IonSegmentButton value="male">low</IonSegmentButton>
+              <IonSegmentButton value="female">medium</IonSegmentButton>
+              <IonSegmentButton value="other">high</IonSegmentButton>
+            </IonSegment>
+          </div>
+          <div className="mt-4 flex">
+            <div className="text-gray-700 mb-1 flex-1">
+              <div className="mb-1">Age</div>
+              <NumericInput
+                value={25}
+                min={16}
+                max={100}
+                step={1}
+                precision={0}
+                size={4}
+                mobile
+              />
+            </div>
+            <div className="text-gray-700 mb-1 flex-1">
+              <div className="mb-1">Age Visibility</div>
+              <IonSegment>
+                <IonSegmentButton value="female">hide</IonSegmentButton>
+                <IonSegmentButton value="other">show</IonSegmentButton>
+              </IonSegment>
+            </div>
+          </div>
+          <div className="mt-4">
+            <div className="text-gray-700 mb-1">
+              <>Favourite Workout Place</>
+              <IonItem className="ion-float-left w-full ion-no-padding">
+                <IonInput />
+              </IonItem>
+            </div>
+          </div>
+        </div>
+        <div className="h-8" />
+
+        <IonList></IonList>
 
         <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
           <IonRefresherContent></IonRefresherContent>
@@ -60,10 +125,10 @@ const ProfileTab: React.FC = () => {
 };
 
 const doRefresh = (event: CustomEvent<RefresherEventDetail>) => {
-  console.log("Begin async operation");
+  console.log('Begin async operation');
 
   setTimeout(() => {
-    console.log("Async operation has ended");
+    console.log('Async operation has ended');
     event.detail.complete();
   }, 2000);
 };
